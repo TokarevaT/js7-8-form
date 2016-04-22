0ff5a2b57fcff6ea04aef75c3c617cd5d9d9c93f
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-04-22 15:43:48 +0300

    added form with error

---
+++ js7-8/js/JavaScript1.js
@@ -0,0 +1,49 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMContentLoaded');
+})
+
+$(function () {
+    var form = '<div class="forms"></div>';
+
+    $('body').append(form);
+
+    var names = [
+        { name: 'First Name', tooltip: "Please provide your firstname" },
+        { name: 'Last Name', tooltip: "Please provide also your lastname" },
+        { name: 'Address', tooltip: "Your home or work address" },
+    ];
+
+    $.each(names, function (index, value) {
+        var div = '<div class="row">' +
+                         '<div class="cols">' +
+                             '<label class="name">' + value.name + '</label>' +
+                             '<div class="text"><input type="text" tooltip="' + value.tooltip + '"/></div>' +
+                          '</div>' +
+                     '</div>';
+        $('.forms').append(div);
+    });
+
+
+    $("input").hover(function () {   
+        var tooltip = $(this).attr("tooltip");
+      $(this).after($("<span> " + tooltip + "</span>"));
+  }, function () {
+      $(this).find("span:last").remove();
+  });
+});
+
+
+var button = '<button></button>';
+$('body').append(button);
+$(function () {
+    $("<button>")
+      .text("Show help")
+      .click(function () {
+          tooltip("open");
+      })
+      .insertAfter("forms");
+})
+
+
+
+
